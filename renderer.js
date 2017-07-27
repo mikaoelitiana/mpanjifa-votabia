525f9b2f9adfd1481c322e38204e356b83845cc8
Mika Andrianarijaona <mika@tag-ip.com>
2017-07-27 17:04:39 +0300

    create main app

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const io = require('socket.io-client');
+
+const socket = io('http://192.168.100.25:8012');
+
+socket.on('pomodoro', function(data) {
+  document.getElementById("timer").innerHTML = data.timer;
+  document.getElementById("status").innerHTML = data.status;
+  if (data.status != 'WORK') {
+    document.getElementById("main").className = "pause";
+  } else {
+    document.getElementById("main").className = "work";
+  }
+})
