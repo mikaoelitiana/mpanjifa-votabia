261cc01675ebf2211d794fd42d088708fd9a173f
Mika Andrianarijaona <mika@tag-ip.com>
2017-08-04 15:47:42 +0300

    change app window title

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 360, height: 200, titleBarStyle: 'hidden'})
 
@@ -22,35 +22,33 @@ function createWindow () {
     slashes: true
   }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
+  // Open the DevTools. mainWindow.webContents.openDevTools() Emitted when the
+  // window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    // Dereference the window object, usually you would store windows in an array if
+    // your app supports multi windows, this is the time when you should delete the
+    // corresponding element.
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// This method will be called when Electron has finished initialization and is
+// ready to create browser windows. Some APIs can only be used after this event
+// occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // On OS X it is common for applications and their menu bar to stay active until
+  // the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // On OS X it's common to re-create a window in the app when the dock icon is
+  // clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow()
   }
